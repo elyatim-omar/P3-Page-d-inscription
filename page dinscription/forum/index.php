785afed7e20ba4f0d0361c18629f35cb36855a9b
elyatim-omar <elyatim.omar.dev@gmail.com>
2021-03-31 10:45:00 +0100

    version 2

--- page dinscription/forum/index.php
+++ page dinscription/forum/index.php
@@ -18,29 +18,15 @@
      <img src="laptop.gif">
     </div>
         <form action="result.php" method="post" class = "col-md-6 col-sm-12 float-right  " >
-            <div class="form-row">
-              <div class="form-group col-md-6">
+              <div class="form-group ">
                 <label for="inputName">Name</label>
                 <input type="text" class="form-control" name="prenom" placeholder="Name">
                 <div><p id=nomr></p></div>
               </div>
-              <div class="form-group col-md-6">
+              <div class="form-group ">
                 <label for="inputSurname4">Surname</label>
                 <input type="text" class="form-control" name="nom" placeholder="Surname">
                 <div><p id=prenomr></p></div>
-              </div>
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                    <div><p id=adressr></p></div>
-                  </div>
-                <div class="form-group col-md-6">
-                  <label for="inputPhone">Phone number</label>
-                  <input type="text" class="form-control" id="inputPhone" placeholder="Phone">
-                  <div><p id=phoner></p></div>
-                </div>
               </div>
             <div class="form-group">
                 <label for="inputEmail">Email</label>
@@ -49,7 +35,7 @@
               </div>
             <button type="submit" class="btn btn-primary" onclick="validation()">Submit</button>
 <a class="btn btn-outline-light" href="result.php">list of participants</a>
-<a class="btn btn-outline-light" href="certificate.php">Certificates</a>
+
 
           </form>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
