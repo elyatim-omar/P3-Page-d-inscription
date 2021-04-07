3891a3ae11c70fe6683e9439908e2d8760d45af3
elyatim-omar <elyatim.omar.dev@gmail.com>
2021-04-07 13:05:54 +0100

    protoype version 2

--- page dinscription/Prototype/traitement.php
+++ page dinscription/Prototype/traitement.php
@@ -1,3 +1,26 @@
+<?php
+//initialisation du session
+session_start();
+
+//initialisation de tableau des participants
+$liste_participants1=[];
+
+ // Trouver ou créer le tableau dans Session
+ if( isset( $_SESSION['liste_participants1'] ) ) {
+    $liste_participants1 =  $_SESSION['liste_participants1'] ;
+ }else {
+    $_SESSION['liste_participants1'] =  $liste_participants1;
+ }
+
+  // Ajouter le nom du nom dans le tableau
+ @ $liste_participants1[] = $_GET["nom"];
+
+  // Enregistrer le tableau dans la session
+$_SESSION['liste_participants1'] =  $liste_participants1;
+
+?>
+
+
 
 
 <!doctype html>
@@ -5,48 +28,20 @@
 <head>
 <meta charset="utf-8" />
 <title>
-Test Formulaire PHP
+ Formulaire v2
 </title>
 </head>
 <body>
-<style>
-table,
-td {
-    border: 1px solid #333;
-}
 
-thead,
-tfoot {
-    background-color: #457798;
-    color: #fff;
-}
-</style>
-
-<table>
-    <thead>
-        <tr>
-            <th colspan="2">Visiteur</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Nom</td>
-            <td>Age</td>
-        </tr>
-        <tr>
-            <td><?php echo $_GET['nom'] ?></td>
-            <td><?php echo $_GET['age'] ?></td>
-        </tr>
-    </tbody>
-</table> </td>
-
-
-  
-
-
-
-
+<h1>la listes des participants</h1>
+<?php
+        foreach ($liste_participants1 as $value) { 
+            echo $value . "<br>"
+        ?>
+        
+        <?php
+        }
+    ?> 
 </body>
 
 </html>
-
